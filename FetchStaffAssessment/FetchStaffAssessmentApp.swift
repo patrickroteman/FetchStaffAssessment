c9dda892f8740acab4c0ac56407e4b6440a4c860
Patrick Roteman <patrickroteman@gmail.com>
2024-11-15 16:35:50 -0800

    Add initial project generation

---
+++ FetchStaffAssessment/FetchStaffAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchStaffAssessmentApp.swift
+//  FetchStaffAssessment
+//
+//  Created by Patrick Roteman on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchStaffAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchStaffAssessmentTests/FetchStaffAssessmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchStaffAssessmentTests.swift
+//  FetchStaffAssessmentTests
+//
+//  Created by Patrick Roteman on 11/15/24.
+//
+
+import Testing
+@testable import FetchStaffAssessment
+
+struct FetchStaffAssessmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
